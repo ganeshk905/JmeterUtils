6f271a544469d571557594c5e6b0db9d5094d24b
sarkershantonu <shantonu_oxford@yahoo.com>
2016-06-06 03:07:01 +0200

    initial project

---
+++ JmxCreator/src/main/java/JmeterTestRunner.java
@@ -0,0 +1,13 @@
+/**
+ * Created by shantonu on 6/6/16.
+ * This run the Jmeter via main
+ */
+public class JmeterTestRunner {
+
+    public static void main(String... args){
+        new JmeterTestRunner().run();
+    }
+    public void run(){
+
+    }
+}
