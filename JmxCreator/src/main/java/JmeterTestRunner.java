74ba4d34f7e1ee6394a9225f18a5dc4ed8afa662
sarkershantonu <shantonu_oxford@yahoo.com>
2016-06-13 00:03:58 +0200

    refactored

--- JmxCreator/src/main/java/JmeterTestRunner.java
+++ JmxCreator/src/main/java/JmeterTestRunner.java
@@ -8,6 +8,7 @@ import org.apache.jmeter.save.SaveService;
 import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jorphan.collections.HashTree;
 import org.apache.poi.util.SystemOutLogger;
+import utils.JmeterEngineUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -28,62 +29,13 @@ public class JmeterTestRunner {
     }
 
     public static void runLocalJmx() {
-        jmeter = initLocalJmeter();
-        start(jmeter);
+        jmeter = JmeterEngineUtils.initLocalJmeter();
+        JmeterEngineUtils.start(jmeter);
 
     }
 
     public void runWithJmx(String jmxPath, String propertyPath, String homePath) {
     }
 
-    public static void start(JMeterEngine engine) {
-        try {
-            engine.runTest();
-        } catch (JMeterEngineException e) {
-            e.printStackTrace();
-        }
-    }
 
-    public static void stop(JMeterEngine engine) {
-        try {
-            engine.stopTest(true);
-        }catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
-
-
-    public static JMeterEngine initLocalJmeter(){
-        String home = System.getProperty("user.dir")+"/src/main/resources";
-        System.out.println(home);
-        return initLocalJmeter(home+"/bin/jmeter.properties",home);
-    }
-    public static JMeterEngine initLocalJmeter(String propertyPath, String homePath){
-        StandardJMeterEngine engine = new StandardJMeterEngine();
-        if(homePath.equals(null)){
-            homePath=System.getProperty("user.dir"+"/src/main/resources");
-        }
-        if(propertyPath.equals(null)){
-            propertyPath = homePath+File.separator+"bin"+File.separator+"jmeter.properties";
-        }
-
-        JMeterUtils.loadJMeterProperties(propertyPath);
-        JMeterUtils.setJMeterHome(homePath);
-        JMeterUtils.initLogging();
-        JMeterUtils.initLocale();
-        try {
-            SaveService.loadProperties();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return engine;
-    }
-
-    public static JMeterEngine loadJmx(JMeterEngine engine, String path) throws IOException {
-            //FileInputStream fis = new FileInputStream(new File(path)); => old way, you need to close
-        HashTree jmx =  SaveService.loadTree(new File(path));
-        engine.configure(jmx);
-        return engine;
-    }
 }
--- JmxCreator/src/main/java/script/ScriptGenerator.java
+++ JmxCreator/src/main/java/script/ScriptGenerator.java
@@ -2,6 +2,7 @@ package script;
 
 import org.apache.jmeter.reporters.ResultCollector;
 import org.apache.jmeter.reporters.Summariser;
+import org.apache.jmeter.testelement.TestPlan;
 import org.apache.jmeter.util.JMeterUtils;
 import org.apache.jorphan.collections.HashTree;
 
@@ -9,7 +10,7 @@ import org.apache.jorphan.collections.HashTree;
  * Created by shantonu on 6/12/16.
  */
 public class ScriptGenerator {
-
+    private static HashTree jmxTree = null;
     public static HashTree addResultINJMX(HashTree jmx, String jtlPath){
         Summariser summary = new Summariser();
         String name = JMeterUtils.getPropDefault("summriser.name", "summary");
@@ -17,5 +18,21 @@ public class ScriptGenerator {
         rc.setFilename(jtlPath);
         jmx.add(jmx.getArray()[0],rc);
         return jmx;
+    }
+    private static HashTree getTestPlan() {
+        if(jmxTree==null){
+            jmxTree = new HashTree();
+        }
+        return jmxTree;
+    }
+    public static HashTree createTestPlan(String name){
+        HashTree testPlanTree = getTestPlan();
+        TestPlan testPlan = new TestPlan(name);
+        jmxTree.add("testPlan", testPlan);
+        //jmxTree.add("loopController",getLoopController("Loop",5));
+        jmxTree.add("threadGroup",getThreadGroup("MyThreadGroup",20, 600, getLoopController("Loop",200)));
+        jmxTree.add("httpSampler",getAnHttp("Google"));
+        return testPlanTree;
+
     }
 }
