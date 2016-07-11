4c329112f2b56c7e3ba18c63f3439c8700afce11
sarkershantonu <shantonu_oxford@yahoo.com>
2016-07-12 01:58:53 +0200

    todo added

--- Jmeter3/src/main/java/org/automation/Appplication.java
+++ Jmeter3/src/main/java/org/automation/Appplication.java
@@ -1,18 +1,39 @@
 package org.automation;
 
+import org.apache.jmeter.NewDriver;
 import org.apache.jmeter.util.ShutdownClient;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
  * Created by shantonu on 7/12/16.
+ * todo
  */
 public class Appplication {
     public static void main(String[] args) {
-        try {
-            ShutdownClient.main(args);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        //NewDriver.main(args);
+        createFolders();
     }
+
+    public static void createJmeterFileStructure() {
+
+    }
+
+    public static void createFolders() {
+        new File(System.getProperty("user.dir") + "/bin").mkdir();
+        new File(System.getProperty("user.dir") + "/lib").mkdir();
+        new File(System.getProperty("user.dir") + "/lib" + "/ext").mkdir();
+        new File(System.getProperty("user.dir") + "/lib" + "/junit").mkdir();
+    }
+
+    //copy all maven items to lib & bin
+    private static void downloadResources(){
+
+    }
+
+    private static void makeJmeterProperties(){
+
+    }
+
 }
