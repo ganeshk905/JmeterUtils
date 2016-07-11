fde669982238679b95507065cd48ee73b1a45c02
sarkershantonu <shantonu_oxford@yahoo.com>
2016-07-12 01:45:51 +0200

    initial project

--- Jmeter3/src/main/java/org/automation/Appplication.java
+++ Jmeter3/src/main/java/org/automation/Appplication.java
@@ -1,10 +1,18 @@
 package org.automation;
 
+import org.apache.jmeter.util.ShutdownClient;
+
+import java.io.IOException;
+
 /**
  * Created by shantonu on 7/12/16.
  */
 public class Appplication {
     public static void main(String[] args) {
-
+        try {
+            ShutdownClient.main(args);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
