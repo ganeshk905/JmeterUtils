1a6524879e218615eeb35f7ca0c7b44d77099c13
sarkershantonu <shantonu_oxford@yahoo.com>
2016-07-30 23:57:21 +0200

    exampel regix

---
+++ Jmeter API Example/Post Processors/src/main/java/org/automation/jmeter/RegularExpressionExtractorExamples.java
@@ -0,0 +1,29 @@
+package org.automation.jmeter;
+
+import org.apache.jmeter.functions.InvalidVariableException;
+import org.apache.jmeter.functions.RegexFunction;
+import org.apache.jmeter.protocol.http.sampler.HTTPSampler;
+import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.samplers.Sampler;
+
+/**
+ * Created by shantonu on 7/30/16.
+ */
+public class RegularExpressionExtractorExamples implements In_JSR223_Java{
+
+
+    @Override
+    public void writeInJSRElement() {
+
+        RegexFunction regix = new RegexFunction();
+        SampleResult result = new SampleResult();
+        Sampler sampler = new HTTPSampler();//this part wont be in there as you will get current samperls
+        String resultRegix;
+        try {
+            resultRegix = regix.execute(result,sampler);
+        } catch (InvalidVariableException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
