ac8d4ce58e7ce0d889f535bdd03d80525ab74925
sarkershantonu <shantonu_oxford@yahoo.com>
2016-08-02 03:29:00 +0200

    Complete Running Jmeter Plugin Example (corrected cook book example)

--- Jmeter Plugin Examples/CookBookExample/src/main/java/com/apache/jmeter/protocol/sftp/sampler/SFTPSamplerBeanInfo.java
+++ Jmeter Plugin Examples/CookBookExample/src/main/java/com/apache/jmeter/protocol/sftp/sampler/SFTPSamplerBeanInfo.java
@@ -13,94 +13,97 @@ import java.beans.PropertyDescriptor;
  */
 public class SFTPSamplerBeanInfo extends BeanInfoSupport {
     private static final Logger LOGGER = LoggingManager.getLoggerForClass();
+
     public SFTPSamplerBeanInfo() {
 
 
         super(SFTPSampler.class);
-        try{
-        createPropertyGroup("Server", // $NON-NLS-1$
-                new String[]{
-                        "hostname", // $NON-NLS-1$
-                        "port", // $NON-NLS-1$
-                        "connectionTimeout"
-                });
+        try {
+            PropertyDescriptor p;
+            createPropertyGroup("Server", // $NON-NLS-1$
+                    new String[]{
+                            "hostname", // $NON-NLS-1$
+                            "port", // $NON-NLS-1$
+                            "connectionTimeout"
+                    });
 
-        createPropertyGroup("Login", // $NON-NLS-1$
-                new String[]{
-                        "username", // $NON-NLS-1$
-                        "password" // $NON-NLS-1$
-                });
+            p = property("hostname"); // $NON-NLS-1$
+            p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+            p.setValue(DEFAULT, "");
 
-        createPropertyGroup("Passphrase",
-                new String[]{
-//                        "sshkeyfile", // $NON-NLS-1$
-                        "passphrase" // $NON-NLS-1$
-                });
+            p = property("port"); // $NON-NLS-1$
+            p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+            p.setValue(DEFAULT, new Integer(22));
 
-        createPropertyGroup("File Transfer", new String[]{
-                "action", // $NON-NLS-1$
-                "source", // $NON-NLS-1$
-                "printFile",// $NON-NLS-1$
-                "destination" // $NON-NLS-1$
+            p = property("connectionTimeout");
+            p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+            p.setValue(DEFAULT, new Integer(5000));
 
-        });
+            createPropertyGroup("Login", // $NON-NLS-1$
+                    new String[]{
+                            "username", // $NON-NLS-1$
+                            "password" // $NON-NLS-1$
+                    });
 
-        PropertyDescriptor p;
-        p = property("username"); // $NON-NLS-1$
-        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
-        p.setValue(DEFAULT, "");
+            p = property("username"); // $NON-NLS-1$
+            p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+            p.setValue(DEFAULT, "");
 
-        p = property("password"); // $NON-NLS-1$
-        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
-        p.setValue(DEFAULT, "");
+            p = property("password"); // $NON-NLS-1$
+            p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+            p.setValue(DEFAULT, "");
 
-        p = property("sshkeyfile");
-        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
-        p.setValue(DEFAULT, "");
-        p.setPropertyEditorClass(FileEditor.class);
+            createPropertyGroup("Passphrase",
+                    new String[]{
+                            //"sshkeyfile", // $NON-NLS-1$
+                            "passphrase" // $NON-NLS-1$
+                    });
 
-        p = property("passphrase");
-        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
-        p.setValue(DEFAULT, "");
 
-        p = property("hostname"); // $NON-NLS-1$
-        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
-        p.setValue(DEFAULT, "");
+           /* p = property("sshkeyfile");
+            p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+            p.setValue(DEFAULT, "");*/
+            p.setPropertyEditorClass(FileEditor.class);
 
-        p = property("port"); // $NON-NLS-1$
-        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
-        p.setValue(DEFAULT, new Integer(22));
+            p = property("passphrase");
+            p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+            p.setValue(DEFAULT, "");
 
-        p = property("connectionTimeout");
-        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
-        p.setValue(DEFAULT, new Integer(5000));
 
-        p = property("action"); // $NON-NLS-1$
-        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
-        p.setValue(NOT_OTHER, Boolean.TRUE);
-        p.setValue(DEFAULT, "get");
-        p.setValue(TAGS, new String[]{
-                SFTPSampler.SFTP_COMMAND_GET,
-                SFTPSampler.SFTP_COMMAND_PUT,
-                SFTPSampler.SFTP_COMMAND_RM,
-                SFTPSampler.SFTP_COMMAND_RMDIR,
-                SFTPSampler.SFTP_COMMAND_LS
-        });
+            createPropertyGroup("FileTransfer", new String[]{
+                    "action", // $NON-NLS-1$
+                    "source", // $NON-NLS-1$
+                    "printFile",// $NON-NLS-1$
+                    "destination" // $NON-NLS-1$
 
-        p = property("source"); // $NON-NLS-1$
-        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
-        p.setValue(DEFAULT, "");
+            });
 
-        p = property("printFile"); // $NON-NLS-1$
-        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
-        p.setValue(DEFAULT, Boolean.TRUE);
+            p = property("action"); // $NON-NLS-1$
+            p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+            p.setValue(NOT_OTHER, Boolean.TRUE);
+            p.setValue(DEFAULT, "get");
+            p.setValue(TAGS, new String[]{
+                    SFTPSampler.SFTP_COMMAND_GET,
+                    SFTPSampler.SFTP_COMMAND_PUT,
+                    SFTPSampler.SFTP_COMMAND_RM,
+                    SFTPSampler.SFTP_COMMAND_RMDIR,
+                    SFTPSampler.SFTP_COMMAND_LS
+            });
 
-        p = property("destination"); // $NON-NLS-1$
-        p.setValue(NOT_UNDEFINED, Boolean.TRUE);
-        p.setValue(DEFAULT, "");
-    } catch (NoSuchMethodError e) {
-        LOGGER.error("Error initializing component GraphGeneratorListener due to missing method, if your version is lower than 2.10, this" +
-                "is expected to fail, if not check project dependencies");
-    }
+            p = property("source"); // $NON-NLS-1$
+            p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+            p.setValue(DEFAULT, "");
+
+            p = property("printFile"); // $NON-NLS-1$
+            p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+            p.setValue(DEFAULT, Boolean.TRUE);
+
+            p = property("destination"); // $NON-NLS-1$
+            p.setValue(NOT_UNDEFINED, Boolean.TRUE);
+            p.setValue(DEFAULT, "");
+        } catch (NoSuchMethodError e) {
+            LOGGER.error("Error initializing component GraphGeneratorListener due to missing method, if your version is lower than 2.10, this" +
+                    "is expected to fail, if not check project dependencies");
+        }
     }
 }
